043246347c2a6293fe5508fef5fa9eb8d43fb320
Francis Visoiu Mistrih <visoiu_f@epita.fr>
2016-08-04 13:52:11 +0200

    matcher: add pretty error diagnostics

--- CastMatcher.cpp
+++ CastMatcher.cpp
@@ -1,13 +1,11 @@
 #include "clang/ASTMatchers/ASTMatchFinder.h"
 #include "clang/ASTMatchers/ASTMatchers.h"
+#include "clang/Basic/Diagnostic.h"
 #include "clang/Frontend/FrontendActions.h"
 #include "clang/Tooling/CommonOptionsParser.h"
 #include "clang/Tooling/Tooling.h"
-#include "llvm/ADT/SmallVector.h"
 #include "llvm/Support/CommandLine.h"
-#include "llvm/Support/raw_ostream.h"
 
-using namespace llvm;
 using namespace clang;
 using namespace clang::ast_matchers;
 using namespace clang::tooling;
@@ -16,38 +14,37 @@ class CastAlert : public MatchFinder::MatchCallback {
 public:
   virtual void run(const MatchFinder::MatchResult &Result) {
     if (const CStyleCastExpr *CE =
-            Result.Nodes.getNodeAs<clang::CStyleCastExpr>("cast"))
-      Found.insert(CE);
-  }
+            Result.Nodes.getNodeAs<clang::CStyleCastExpr>("cast")) {
 
-  SmallPtrSet<const CStyleCastExpr *, 4> Found;
+      auto &SourceManager = *Result.SourceManager;
+
+      // Report it as an error.
+      auto &DiagEngine = SourceManager.getDiagnostics();
+      auto DiagID = DiagEngine.getDiagnosticIDs()->getCustomDiagID(
+          DiagnosticIDs::Error, "explicit cast detected");
+      // Add a removal hint.
+      DiagEngine.Report(CE->getLocStart(), DiagID)
+          << FixItHint::CreateRemoval({CE->getLocStart(), CE->getLocEnd()});
+    }
+  }
 };
 
 static llvm::cl::OptionCategory CastMatcherCategory("cast-matcher options");
-static cl::extrahelp CommonHelp(CommonOptionsParser::HelpMessage);
-static cl::extrahelp MoreHelp("\nThis tool helps the detection of explicit"
+static llvm::cl::extrahelp CommonHelp(CommonOptionsParser::HelpMessage);
+static llvm::cl::extrahelp MoreHelp("\nThis tool helps the detection of explicit"
                               "C-style casts.");
 
 int main(int argc, const char **argv) {
   CommonOptionsParser OptionsParser(argc, argv, CastMatcherCategory);
+  CastAlert Alert;
   ClangTool Tool(OptionsParser.getCompilations(),
                  OptionsParser.getSourcePathList());
 
-  CastAlert Alert;
   MatchFinder Finder;
+
+  // Match all explicit casts in the main file (exclude system headers).
   Finder.addMatcher(
       cStyleCastExpr(unless(isExpansionInSystemHeader())).bind("cast"), &Alert);
 
-  auto ret = Tool.run(newFrontendActionFactory(&Finder).get());
-
-  PrintingPolicy policy{LangOptions{}};
-  for (auto *CE : Alert.Found) {
-    llvm::outs() << "Cast found: \n";
-    CE->printPretty(llvm::outs(), nullptr, policy);
-    llvm::outs() << '\n';
-  }
-
-  if (Alert.Found.size())
-    return 1;
-  return ret;
+  return Tool.run(newFrontendActionFactory(&Finder).get());
 }
